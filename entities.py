36cd56e2a7b4ecbf0ef6ec9d7512c57a6e15a5d3
br-nch <isaacanderson@outlook.com>
2022-12-12 09:10:15 -0800

    Entities and Players

---
+++ entities.py
@@ -0,0 +1,14 @@
+
+class entity:
+    def __init__(self, sprite, x, y, w, h):
+        self.sprite = sprite
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+    
+    def show(self, rot):
+        pass
+
+    def move(self, v, dir):
+        pass
---
+++ player.py
@@ -0,0 +1,8 @@
+
+class player:
+
+    def __init__(self):
+        self.x = 0 #tmp
+        self.y = 0 #tmp
+        self.gold = 0 #tmp
+        self.rot = 0 #tmp
