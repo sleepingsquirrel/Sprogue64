05e82cf61a1938bb0ca7b042e11be4cf2e8c75ea
Nate Mitchell <natemitchell1256@gmail.com>
2022-12-16 09:42:32 -0800

    requirements

--- shaders.py
+++ shaders.py
@@ -26,7 +26,13 @@ class shader_storage:
             FragColor = vec4(0.5,0.5,0.5,1.0);
         }
         else {
-            FragColor = vec4(1.0,1.0,1.0,1.0);
+            if (tex.y < 0.5){
+                FragColor = vec4(1.0,1.0,1.0,1.0);
+            }
+            else{
+                FragColor = vec4(0.0,0.0,0.0,1.0);
+            }
+           
         }
     } 
     
