1b16962416205c0bec443348c29e9c10433d2250
br-nch <isaacanderson@outlook.com>
2023-01-04 08:45:11 -0800

    this.

--- dev.py
+++ dev.py
@@ -1,13 +1,9 @@
 from world import *
 
 def maker():
-    while True:
-        id = input("Object Type: ").strip().lower()
-        if (id in types):
-            break
-    while True:
-        info = input("Info: ").strip().lower().split(" ")
-    
+    id = input("Object Type: ").strip().lower()
+    info = input("Info: ").strip().lower().split(" ")
+        
 
 
 if __name__ == "__main__":
