d1801e771e60cb062fef42f45c2918d0fc40723c
Nate Mitchell <natemitchell1256@gmail.com>
2023-01-13 10:01:32 -0800

    preloader

--- preload.py
+++ preload.py
@@ -1,14 +1,29 @@
 import csv
 from world import wall
 from math import hypot
-def load_csv(self):
+import pickle
+
+def preload(map = "map.csv",level_name = "level.spr"):
+    with open(level_name, "ab") as file:
+        pickle.dump(load_csv('map.csv'), file)
+
+def load_csv(map):
     shapes = []
-    with open('map.csv') as file:
+    with open(map) as file:
         reader = csv.DictReader(file)
         for i in reader:
-            current = wall(i.id, i.r, i.x, i.y, i.w, i.h)
-            shapes.append(current)
+            shapes.append(make_shape(i))
         return shapes
+
+def make_shape(obj):
+    match obj.id:
+        case 1:
+            return circle_convert(obj.x,obj.y,obj.w)
+        case 2:
+            return line_convert(obj.x,obj.y,obj.w,obj.h)
+        case 3:
+            return semicircle_convert(obj.x,obj.y,obj.w,obj.h,obj.rot)
+
 def semicircle_convert(x1,y1,x2,y2,r):
     #get middle
     cx,cy = (x1+x2)/2,(y1+y2)/2
@@ -16,17 +31,17 @@ def semicircle_convert(x1,y1,x2,y2,r):
     return wall(3,r,cx,cy,rad,0)
 
 def line_convert(x1,y1,x2,y2):
-    
-    w = wall()
+    is_horizontal = int((x1-x2) < (y1-y2))
+    p1 = [x1,y1]
+    p2 = [x2,y2]
+    mid = [(p1[is_horizontal]+p2[is_horizontal])/2,p1[int(not is_horizontal)]]
+    r = abs(p1[is_horizontal]-p2[is_horizontal]) /2
+    w = wall(2,is_horizontal,mid[0],mid[1],r,None)
+
+def circle_convert(x,y,r):
+    return wall(1,0,x,y,r,0)
 
 
 
-
-# opening the CSV file
-with open('world.csv', mode ='r') as file:
-
-    # reading the CSV file
-    reader = csv.DictReader(file)
-
-    # displaying the contents of the CSV file
-    for obj in reader:
+print(__name__)
+preload()
