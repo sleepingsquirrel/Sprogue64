aeb8b638c405eb2cfcfc87fd20a1c255446e0d8e
br-nch <isaacanderson@outlook.com>
2023-01-20 09:07:10 -0800

    CROZHAIRS

--- entities.py
+++ entities.py
@@ -1,12 +1,13 @@
 from math import sin, cos
 
 class entity:
-    def __init__(self, sprite, x, y, w, h):
+    def __init__(self, sprite, x, y, w, h, hp=100):
         self.sprite = sprite
         self.x = x
         self.y = y
         self.w = w
         self.h = h
+        self.hp = hp
     
     def show(self, rot):
         pass
