d52647577bf14a1a31c6d288cf70a26b0e87b41f
Nate mitchell <natemitchell1256@gmail.com>
2023-01-25 16:25:33 -0800

    enm atlas

--- entities.py
+++ entities.py
@@ -1,6 +1,6 @@
 from math import sin, cos, atan, dist
 import player
-
+from pil import Image
 class entity:
     def __init__(self, sprite, x, y, w, h, hp=100):
         self.sprite = open(sprite)
@@ -23,3 +23,21 @@ class entity:
         if dist([target.x, target.y], [self.x, self.y]) < self.w + target.w:
             return True
         return False
+
+
+class texture_atlas:
+    def __init__(self,loc):
+        
+        for i, filename in enumerate(os.listdir("assets/")):
+            if "." in filename:
+                if filename.split('.')[1] == "gsm":
+                    texlen += 1
+        self.atlas = numpy.zeros(1000*texlen,1000,4)
+        for i, filename in enumerate(os.listdir("assets/")):
+            with Image.open("assets/" + filename) as img:
+                pix = img.load()
+                assert img.size[0] == 1000
+                assert img.size[1] == 1000
+                self.atlas[1000*i:1000*i+1000,0:1000] = pix
+
+        
